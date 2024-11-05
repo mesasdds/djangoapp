9ab98e02292c668630dbd19aa2c0eba3ab45a1e5
= <=>
2024-11-05 01:39:58 -0300

    feat: delete created

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -41,6 +41,12 @@ def editTask(request, id):
         return render(request, 'tasks/edittask.html', {'form':form, 'task':task})
 
 
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    return redirect('/')
+
 def helloWorld(request):
     return HttpResponse('Hello World')
 
