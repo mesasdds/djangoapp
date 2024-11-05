d5e1e44c6c2591f29878cc29677a563bfd5bac35
= <=>
2024-11-05 01:47:49 -0300

    feat: message info when user del task

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 def taskList(request):
     tasks = Task.objects.all().order_by('-created_at')
@@ -45,6 +46,9 @@ def editTask(request, id):
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
+    
+    messages.info(request, 'Tarefa deletada com sucesso.')
+
     return redirect('/')
 
 def helloWorld(request):
