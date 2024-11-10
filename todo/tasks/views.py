09070d299461712137702055288b8431a35cd04f
= <=>
2024-11-10 07:32:34 -0300

    feat: added search and minor pagination CSS fix

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -6,12 +6,21 @@ from .forms import TaskForm
 from django.contrib import messages
 
 def taskList(request):
-    tasks_list = Task.objects.all().order_by('-created_at')
-    paginator = Paginator(tasks_list, 3)
 
-    page = request.GET.get('page')
+    search = request.GET.get('search')
 
-    tasks = paginator.get_page(page)
+    if search:
+        
+        tasks = Task.objects.filter(title__icontains=search)
+
+    else:
+
+        tasks_list = Task.objects.all().order_by('-created_at')
+        paginator = Paginator(tasks_list, 3)
+
+        page = request.GET.get('page')
+
+        tasks = paginator.get_page(page)
 
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
